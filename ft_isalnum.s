b3ff09cff6bc2e832906f4d126d418612155e0d7
Brahim Hamidi <bhamidi@student.42.fr>
2018-07-24 18:58:57 +0200

    wip

--- ft_isalnum.s
+++ ft_isalnum.s
@@ -6,17 +6,13 @@
 _ft_isalnum:
 	push	rbx
 	call	_ft_isdigit
-	cmp		rax, 1
-	jne		unmatch
-	call	_ft_isalpha
-	cmp		rax, 1
-	jne		unmatch
-	jmp		done
+	pop		rbx
+	cmp		rax, 0
+	je		unmatch
+	ret
 
 unmatch:
-	mov		rax, 0
-	jmp		done;
-
-done:
+	push	rbx
+	call	_ft_isalpha
 	pop		rbx
 	ret
--- test.c
+++ test.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int		ft_isalnum(int);
+# define FUNCTION ft_isalnum
+
+int		FUNCTION(int);
 
 int main(void)
 {
-	puts("Test for ft_isalnum");
-	for (int i = 40; i < 125; ++i)
-		printf("ft_isalnum(%2d): -> %d\n", i, ft_isalnum(i));
+	puts("Test for FUNCTION");
+	for (int i = -15; i < 140; ++i)
+		printf("FUNCTION(%2d): -> %d\n", i, FUNCTION(i));
 	return (0);
 }
