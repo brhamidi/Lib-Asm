5793d02b9eb082ea55e97dfbc50d0eabd4575ce9
Brahim Hamidi <bhamidi@student.42.fr>
2018-07-28 15:38:00 +0200

    wip

--- ft_cat.s
+++ ft_cat.s
@@ -1,41 +1,48 @@
 	global	_ft_cat
 
 ; ssize_t read(int fildes, void *buf, size_t nbyte)
+; ssize_t write(int fildes, const void *buf, size_t nbyte);
 
 	section	.text
 _ft_cat:
-	mov     rax, 0x02000003
+	cmp		rdi, 0
+	jl		error
+	push	rdi
+	mov     rax, 0x02000003 ; read syscall number
 	lea		rsi, [rel buf]
 	mov		rdx, len
 	syscall
+	pop		rdi
+	jc		error
 	cmp		rax, 0
 	je		end
 	jl		error
 
 work:
 	push	rdi
-	mov     rax, 0x02000004
+	mov		rdx, rax		; set len paramter for write syscall
+	mov     rax, 0x02000004 ; write syscall number
 	mov		rdi, 1
 	lea		rsi, [rel buf]
-	mov		rdx, len
 	syscall
 	pop		rdi
 	mov     rax, 0x02000003
 	lea		rsi, [rel buf]
 	mov		rdx, len
 	syscall
+	jc		error
 	cmp		rax, 0
 	je		end
 	jl		error
 	jmp		work
 
 error:
-	mov		rax, 1
+	mov		rax, -1
 	ret
 
 end:
 	ret
 
 	section		.bss
-len		equ		10
+len		equ		256
 buf:	resb	len
--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void	ft_cat(int);
 
 int		main(void)
 {
-	ft_cat(-1);
 	return (0);
 }
